bd40729d0f80aff671094dbac508df4c40195c74
johnmuthua <caveomuthama@gmail.com>
2021-02-23 16:19:03 +0300

    chore:removed unnessesary react boilerplate

---
+++ src/components/Header/Nav/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from './SideBar'
+import Main from './Main';
+function NavBar() {
+    return (
+        <div>
+            <SideBar />
+            <Main />
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/components/Header/Nav/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SideBar() {
+    return (
+        <div>
+            This is the Sidebar
+        </div>
+    )
+}
+
+export default SideBar
