bc0d51b7369dd8c75a4e9415873ffb1a1c34392e
muthuajohn <caveomuthama@gmail.com>
2021-02-26 15:03:20 +0300

    feat: Added a question component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Topbar from "./components/Header/Navbar";
 import Subsection from "./components/Content/Subsection/Subsection";
+import Hero from "./components/Content/Hero/Hero";
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Hero />
       <Subsection />
     </div>
   );
